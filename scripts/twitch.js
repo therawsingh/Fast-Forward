97f1c09563adef7f4a8d853aaeaa3eea829f4f9b
fLuKeroshan <32307382+fLuKeroshan@users.noreply.github.com>
2023-08-21 21:50:49 +0530

    twitch

--- scripts/twitch.js
+++ scripts/twitch.js
@@ -1,10 +1,6 @@
 window.addEventListener("load", () => {
   const clickBonusButton = () => {
-    const bonusButton = Array.from(document.querySelectorAll("button")).find(
-      (button) => {
-        return button.ariaLabel === "Claim Bonus";
-      }
-    );
+    const bonusButton = document.querySelector('[aria-label="Claim Bonus"]');
 
     if (bonusButton) {
       bonusButton.click();
